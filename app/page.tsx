92ee7e10fc71cac82b557aa091f24485d73ed7cd
Hakemi75 <reou0716@gmail.com>
2025-01-30 01:04:59 +0900

    ♻️ Linkコンポーネントで画面遷移

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-'use client';
 import { Footer } from "../components/footer"
 import { Main } from "../components/main"
+import { Header } from "../components/header"
 
 
 export default function Home(props) {
@@ -9,6 +9,7 @@ const handleClick = () => {
 };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <Main page="index"/>
       <Footer/>
     </div>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import {Footer} from "../../components/footer"
 import {Links} from "../../components/links"
 import {Headline} from "../../components/headline"
+import {Header} from "../../components/header"
 
 export default function Home(props) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <Header />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
