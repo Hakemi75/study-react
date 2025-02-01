52b8444a47752984f8993e4524a507753a71f46a
Hakemi75 <reou0716@gmail.com>
2025-02-01 22:08:35 +0900

    ✨ useStateで文字列と真偽値について解説

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,12 @@ import { useCallback, useEffect, useState } from "react";
 //         alert(foo);
 //       }
 
-export default function Home(props) {
+export default function Home(props:any) {
 const [count, setCount] = useState(1);
+const [text, setText] = useState("");
+const [isShow, setIsShow] = useState(true);
 
-const handleClick2 = useCallback((e) => {
+const handleClick2 = useCallback((e:any) => {
 if (count < 10) {
   // alert('クリック');
   // foo = foo + 1
@@ -42,15 +44,28 @@ useEffect(() => {
     document.body.style.backgroundColor="";
   };
 }, []);
+const handleChange = useCallback((e) => {
+  if (e.target.value.length > 5) {
+    alert("5文字以内にしてください");
+    return;
+  }
+  setText(e.target.value.trim());
+}, []);
 
+const handleDisplay = useCallback(() => {
+        setIsShow((isShow) => !isShow)
+}, []);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <Header />
-      <button href="/products" onClick={handleClick2}>
+      <button onClick={handleClick2}>
       ボタン
       </button>
+      <button onClick={handleDisplay}>
+      {isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange}/>
       <Main page="index"/>
       <Footer/>
     </div>
